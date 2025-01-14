62b6c3bbfdae01b929b3a8910742a0eb41af2815
Bhavik Maneck <bhavikmaneck@microsoft.com>
2025-01-14 12:42:59 +0000

    starter structure, files and working dev container

---
+++ .devcontainer/post_create.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the path to your Zsh profile
+zshrc_path="$HOME/.zshrc"
+bashrc_path="$HOME/.bashrc"
+
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$zshrc_path"
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$bashrc_path"
+
+cat $HOME/.zshrc
+export PATH="$HOME/.local/bin:$PATH"
+
+# install the pre-commit hooks
+pre-commit autoupdate
+pre-commit install
---
+++ src/example.py
@@ -0,0 +1,2 @@
+# print a test msg
+print("Hello World!")
